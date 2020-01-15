8b89d4cd0a8d518cacd8cc898e0f2083c5dafb3a
Sietse Nefs <sietse345@live.nl>
2020-01-15 14:04:54 +0100

    Added changing logo

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,43 @@
-// JavaScript Document
+
+// ==== Logo color switch ================================
+
+function logoSwitch () {
+  $('.altLogo').each(function() {
+    
+    var distance = $('.startLogo').offset().top - $(this).closest('.row').offset().top;
+    
+    TweenLite.to(this,0,{y:distance, force3D:true});
+    
+    //$(this).css('top',
+   //   $('.startLogo').offset().top -  $(this).closest('.row').offset().top
+   // );
+  });
+};
+
+$(document).scroll(function() {logoSwitch();});
+
+logoSwitch();
+
+// ==== Logo animation ================================
+
+console.clear();
+var tl = new TimelineLite({paused: true});
+var dur = .5;
+tl.to(".OO", dur, {drawSVG: 0, ease: Sine.easeInOut});
+tl.fromTo(".OO", dur, 
+      {drawSVG:"100% 100%", ease: Sine.easeIn},
+      {drawSVG: "100% 0%", ease: Sine.easeOut, immediateRender: false})
+
+
+$(".animate").mouseenter(function(){
+  if ( !tl.isActive() ) {
+    tl.play(0);
+  }
+});
+
+
+
+
+
+
+
